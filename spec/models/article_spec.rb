0d65aa1c08ffaa52d6338aeb1b5d0878ba31cc8f
yuta-ushijima <register@yuta-u.com>
2018-08-11 21:57:07 +0900

    article_specの修正

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe "validates" do
-    let(:article) { create(:article) }
+
     it "タイトルが空であるとき、無効であること" do
       article= Article.new(title: nil)
       article.valid?
@@ -11,7 +11,7 @@ RSpec.describe Article, type: :model do
 
     it "本文が空であるとき、無効であること" do
       article= Article.new(body: nil)
-      article.valid?
+      article.save
       expect(article.errors[:body]).to include("を入力してください。")
     end
   end
