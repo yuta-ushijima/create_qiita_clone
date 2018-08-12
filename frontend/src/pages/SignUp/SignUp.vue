70e03070a94488475f696a915a696e62912333a3
yuta-ushijima <register@yuta-u.com>
2018-08-12 15:06:56 +0900

    ユーザー登録ページのルーティングを設定

---
+++ frontend/src/pages/SignUp/SignUp.vue
@@ -0,0 +1,14 @@
+<template src='./signUp.html'></template>
+<style lang='scss' src='./signUp.scss'></style>
+<script>
+  import axios from 'axios'
+  const welcomeEndpoint = process.env.WELCOME_ENDPOINT
+
+  export default {
+    created: () => {
+      axios.get(welcomeEndpoint).then( response => {
+        console.log(response)
+      })
+    }
+  }
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Welcome from '@/pages/Welcome/Welcome'
+import SignUp from '@/pages/SignUp/SignUp'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Welcome',
       component: Welcome
+    },
+    {
+      path: '/signUp',
+      name: 'SignUp',
+      component: SignUp
     }
   ]
 })
