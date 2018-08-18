b94fdd5774cdbecece23d71b4b3b05a23bc7056e
yuta-ushijima <register@yuta-u.com>
2018-08-18 20:33:52 +0900

    seedの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Now Creating User...."
+
+# seeds of User
+100.times do |user|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email = Faker::Internet.email
+  password = Faker::Internet.password(8)
+
+  User.create!(
+          first_name: first_name,
+          last_name: last_name,
+          email: email,
+          password: password,
+  )
+end
+puts "Complete!"
+
+puts "Now Creating Article...."
+
+# seeds of Article
+100.times do |article|
+
+  title = Faker::Markdown.headers
+  body = Faker::Markdown.sandwich(5, 3)
+
+  Article.create!(body: body, title: title, user_id: rand(1..100))
+
+end
+
+puts "Complete!"
