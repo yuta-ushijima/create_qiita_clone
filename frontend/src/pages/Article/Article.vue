c3e0dfc8edc0a8005313eaeb395b4f1c4ab9e095
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2018-08-21 11:04:34 +0900

    対象の記事内容が表示できるように修正
    シングルクオートをダブルクオートで囲む。逆は基本ない。

--- frontend/src/pages/Article/Article.vue
+++ frontend/src/pages/Article/Article.vue
@@ -2,17 +2,18 @@
 <style src="./article.scss"></style>
 <script>
 import axios from 'axios'
+
 const DOMAIN_BASE = process.env.DOMAIN_BASE
+
 export default {
-  props: ['id', 'title', 'body'],
+  props: ['id'],
   data () {
     return {
       article: null
     }
   },
   mounted () {
-    console.log(this.props.id)
-    axios.get(`${DOMAIN_BASE}/articles/${this.props.id}`)
+    axios.get(`${DOMAIN_BASE}/articles/${this.id}`)
       .then(response => {
         this.article = response.data
         console.log(response.data)
