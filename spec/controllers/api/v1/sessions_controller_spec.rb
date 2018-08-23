bf8e0a7f2562416aa44cebce465bb3739b728b19
yuta-ushijima <register@yuta-u.com>
2018-08-23 22:39:08 +0900

    Revert "Revert "sessions_controller_specの作成""
    This reverts commit 7842214f6b6db5ed0795b2ac1d43d07e2b907597.

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -4,15 +4,14 @@ RSpec.describe Api::V1::SessionsController, type: :controller do
   describe "#create" do
     let(:user) { create(:user) }
     it "登録されているユーザーがログインできること" do
-      sign_in user
-      get :create
+      post :create, params: { email: user.email, password: user.password }
       expect(response).to have_http_status :ok
-    end
-
-    it "ログイン後に正しいレスポンスを返すこと" do
-      sign_in user
-      get :create
-      expect(response).to be_successful
+      json = JSON.parse(response.body)
+      aggregate_failures do
+        expect(json["email"]).to eq user.email
+        expect(json["token_type"]).to eq "Bearer"
+        expect(json["access_token"]).to eq user.access_token
+      end
     end
   end
 
