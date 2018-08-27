e70b9ee0cb206ce6ffb8036a8100cbfea6247179
yuta-ushijima <register@yuta-u.com>
2018-08-27 22:20:11 +0900

    ログインして記事一覧ページにリダイレクトさせるまで

--- frontend/src/pages/Login/Login.vue
+++ frontend/src/pages/Login/Login.vue
@@ -1,3 +1,31 @@
 <template src="./login.html"></template>
 <style src="./login.scss"></style>
-<script></script>
+<script>
+import axios from 'axios'
+
+const DOMAIN_BASE = process.env.DOMAIN_BASE
+
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signIn: function () {
+      const params = {
+        email: this.email,
+        password: this.password
+      }
+      axios.post(`${DOMAIN_BASE}/login`, params)
+        .then(response => {
+          // ログインしたら、一覧ページへリダイレクト
+          this.$router.push('/articles')
+          console.log(response.data)
+          console.log(response.status)
+        })
+    }
+  }
+}
+</script>
