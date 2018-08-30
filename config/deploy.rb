9fc3ed9a88f5916a80b8f7fd78b088a21bd90740
yuta-ushijima <register@yuta-u.com>
2018-08-30 22:42:07 +0900

    capistrano settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/var/www/my_app_name"
 set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, %w{config/database.yml config/secrets.yml}
+append :linked_files, %w[config/database.yml config/secrets.yml]
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
@@ -30,8 +30,8 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :default_env, {
-    rbenv_root: "/usr/local/rbenv",
-    path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH"
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
 }
 
 # Default value for local_user is ENV["USER"]
