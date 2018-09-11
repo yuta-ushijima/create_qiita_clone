cbb016582d43fd46b47822fd1f57dd64279eda84
yuta-ushijima <register@yuta-u.com>
2018-09-12 00:53:42 +0900

    bundle exec annotate

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,33 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint(8)        not null, primary key
+#  body       :text(65535)
+#  title      :string(255)
+#  user_id    :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  status     :string(255)
+#
+
 class Article < ApplicationRecord
+  extend Enumerize
+
+  # association
+  belongs_to :user
+
   # validation
   with_options presence: true do
     validates :title
     validates :body
   end
 
-  # association
-  belongs_to :user
+  # enumerize
+  enumerize :status, in: [:draft, :published], default: :draft, scope: true, predicate: true
+
+  # scope
+  scope :active, -> {
+    without_status(:draft)
+  }
 end
