c6b13c57f03ec4ef977d103bc5e5e9cc2a7dd696
yuta-ushijima <register@yuta-u.com>
2018-09-12 00:56:40 +0900

    fix articles_controler

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,43 +1,31 @@
 class Api::V1::ArticlesController < ApplicationController
-  # TODO: Devise の調整が完了したら、:updateと:deleteを外すようにする
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_article, only: [:update, :destroy]
 
   def index
-    @articles = Article.order("created_at desc")
+    @articles = Article.active.order("created_at desc")
     render json: @articles
   end
 
   def show
-    @article = Article.find(params[:id])
-    if @article.present?
-      render json: @article, status: 200
-    else
-      render json: @article, status: 400
-    end
+    @article = Article.active.find(params[:id])
+    render json: @article
   end
 
   def create
-    @article = current_user.articles.new(article_params)
-    if @article.save!
-      render json: @article, serializer: ArticleSerializer, status: 200
-    else
-      render json: @article, status: 400
-    end
+    @article = current_user.articles.create!(article_params)
+    render json: @article, serializer: ArticleSerializer
   end
 
   def update
-    if @article.present?
-      @article.update!(article_params)
-      render json: @article, serializer: ArticleSerializer, status: 200
-    else
-      render json: @article, serializer: ArticleSerializer, status: 400
-    end
+    @article.update!(article_params)
+    render json: @article, serializer: ArticleSerializer
   end
 
   def destroy
     @article.destroy!
-    render json: @article, serializer: ArticleSerializer, status: 204
+    render json: @article, serializer: ArticleSerializer
+    head :no_content
   end
 
   private
@@ -47,7 +35,6 @@ class Api::V1::ArticlesController < ApplicationController
     end
 
     def set_article
-      # TODO: Devise の調整が完了したらcurrent_userから取得するようにする
       @article = current_user.articles.find(params[:id])
     end
 end
