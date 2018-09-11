cf692ce769eead457ed733fe80e8d29cd4ffa59d
yuta-ushijima <register@yuta-u.com>
2018-09-12 00:59:41 +0900

    enumerizeの導入とannotate

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,21 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint(8)        not null, primary key
+#  body       :text(65535)
+#  title      :string(255)
+#  user_id    :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  status     :string(255)
+#
+
 FactoryBot.define do
   factory :article do
     title { Faker::Markdown.headers }
     body { Faker::Markdown.sandwich(6, 4) }
     association :user, factory: :user
+    status { :published }
   end
 end
